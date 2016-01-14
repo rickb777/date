8978bef2207e6e09272c23eabb7b5fa91e48f9ee
Rick <rickb777@github.com>
2016-01-14 19:24:12 +0000

    Bug fix - using "Monday" in a format string clashed with "2nd"

--- format.go
+++ format.go
@@ -6,10 +6,7 @@ package date
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
-	"time"
-	"unicode"
 )
 
 // These are predefined layouts for use in Date.Format and Date.Parse.
@@ -31,187 +28,6 @@ const (
 	RFC1123W = "Mon, 02 Jan 2006" // RFC1123 with day of the week
 	RFC3339  = "2006-01-02"
 )
-
-// MustAutoParse is as per AutoParse except that it panics if the string cannot be parsed.
-// This is intended for setup code; don't use it for user inputs.
-func MustAutoParse(value string) Date {
-	d, err := AutoParse(value)
-	if err != nil {
-		panic(err)
-	}
-	return d
-}
-
-// AutoParse is like ParseISO, except that it automatically adapts to a variety of date formats
-// provided that they can be detected unambiguously. Specifically, this includes the "European"
-// and "British" date formats but not the common US format. Surrounding whitespace is ignored.
-// The supported formats are:
-//
-// * all formats supported by ParseISO
-//
-// * yyyy/mm/dd | yyyy.mm.dd (or any similar pattern)
-//
-// * dd/mm/yyyy | dd.mm.yyyy (or any similar pattern)
-//
-func AutoParse(value string) (Date, error) {
-	abs := strings.TrimSpace(value)
-	sign := ""
-	if value[0] == '+' || value[0] == '-' {
-		abs = value[1:]
-		sign = value[:1]
-	}
-
-	if len(abs) >= 10 {
-		i1 := -1
-		i2 := -1
-		for i, r := range abs {
-			if unicode.IsPunct(r) {
-				if i1 < 0 {
-					i1 = i
-				} else {
-					i2 = i
-				}
-			}
-		}
-		if i1 >= 4 && i2 > i1 && abs[i1] == abs[i2] {
-			// just normalise the punctuation
-			a := []byte(abs)
-			a[i1] = '-'
-			a[i2] = '-'
-			abs = string(a)
-		} else if i1 >= 2 && i2 > i1 && abs[i1] == abs[i2] {
-			// harder case - need to swap the field order
-			dd := abs[0:i1]
-			mm := abs[i1+1 : i2]
-			yyyy := abs[i2+1:]
-			abs = fmt.Sprintf("%s-%s-%s", yyyy, mm, dd)
-		}
-	}
-	return ParseISO(sign + abs)
-}
-
-// MustParseISO is as per ParseISO except that it panics if the string cannot be parsed.
-// This is intended for setup code; don't use it for user inputs.
-func MustParseISO(value string) Date {
-	d, err := ParseISO(value)
-	if err != nil {
-		panic(err)
-	}
-	return d
-}
-
-// ParseISO parses an ISO 8601 formatted string and returns the date value it represents.
-// In addition to the common formats (e.g. 2006-01-02 and 20060102), this function
-// accepts date strings using the expanded year representation
-// with possibly extra year digits beyond the prescribed four-digit minimum
-// and with a + or - sign prefix (e.g. , "+12345-06-07", "-0987-06-05").
-//
-// Note that ParseISO is a little looser than the ISO 8601 standard and will
-// be happy to parse dates with a year longer in length than the four-digit minimum even
-// if they are missing the + sign prefix.
-//
-// Function date.Parse can be used to parse date strings in other formats, but it
-// is currently not able to parse ISO 8601 formatted strings that use the
-// expanded year format.
-//
-// Background: https://en.wikipedia.org/wiki/ISO_8601#Dates
-func ParseISO(value string) (Date, error) {
-	if len(value) < 8 {
-		return Date{}, fmt.Errorf("Date.ParseISO: cannot parse %s: incorrect length", value)
-	}
-
-	abs := value
-	if value[0] == '+' || value[0] == '-' {
-		abs = value[1:]
-	}
-
-	dash1 := strings.IndexByte(abs, '-')
-	fm1 := dash1 + 1
-	fm2 := dash1 + 3
-	fd1 := dash1 + 4
-	fd2 := dash1 + 6
-
-	if dash1 < 0 {
-		// switch to YYYYMMDD format
-		dash1 = 4
-		fm1 = 4
-		fm2 = 6
-		fd1 = 6
-		fd2 = 8
-	} else if abs[fm2] != '-' {
-		return Date{}, fmt.Errorf("Date.ParseISO: cannot parse %s: incorrect syntax", value)
-	}
-	//fmt.Printf("%s %d %d %d %d %d\n", value, dash1, fm1, fm2, fd1, fd2)
-
-	if len(abs) != fd2 {
-		return Date{}, fmt.Errorf("Date.ParseISO: cannot parse %s: incorrect length", value)
-	}
-
-	year, err := parseField(value, abs[:dash1], "year", 4, -1)
-	if err != nil {
-		return Date{}, err
-	}
-
-	month, err := parseField(value, abs[fm1:fm2], "month", -1, 2)
-	if err != nil {
-		return Date{}, err
-	}
-
-	day, err := parseField(value, abs[fd1:], "day", -1, 2)
-	if err != nil {
-		return Date{}, err
-	}
-
-	if value[0] == '-' {
-		year = -year
-	}
-
-	t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
-
-	return Date{encode(t)}, nil
-}
-
-func parseField(value, field, name string, minLength, requiredLength int) (int, error) {
-	if (minLength > 0 && len(field) < minLength) || (requiredLength > 0 && len(field) != requiredLength) {
-		return 0, fmt.Errorf("Date.ParseISO: cannot parse %s: invalid %s", value, name)
-	}
-	number, err := strconv.Atoi(field)
-	if err != nil {
-		return 0, fmt.Errorf("Date.ParseISO: cannot parse %s: invalid %s", value, name)
-	}
-	return number, nil
-}
-
-// MustParse is as per Parse except that it panics if the string cannot be parsed.
-// This is intended for setup code; don't use it for user inputs.
-func MustParse(layout, value string) Date {
-	d, err := Parse(layout, value)
-	if err != nil {
-		panic(err)
-	}
-	return d
-}
-
-// Parse parses a formatted string of a known layout and returns the Date value it represents.
-// The layout defines the format by showing how the reference date, defined
-// to be
-//     Monday, Jan 2, 2006
-// would be interpreted if it were the value; it serves as an example of the
-// input format. The same interpretation will then be made to the input string.
-//
-// This function actually uses time.Parse to parse the input and can use any
-// layout accepted by time.Parse, but returns only the date part of the
-// parsed Time value.
-//
-// This function cannot currently parse ISO 8601 strings that use the expanded
-// year format; you should use date.ParseISO to parse those strings correctly.
-func Parse(layout, value string) (Date, error) {
-	t, err := time.Parse(layout, value)
-	if err != nil {
-		return Date{}, err
-	}
-	return Date{encode(t)}, nil
-}
 
 // String returns the time formatted in ISO 8601 extended format
 // (e.g. "2006-01-02").  If the year of the date falls outside the
@@ -280,6 +96,17 @@ func (d Date) FormatWithSuffixes(layout string, suffixes []string) string {
 		return t.Format(layout)
 
 	default:
+		// If the format contains "Monday", it has been split so repair it.
+		i := 1
+		for i < len(parts) {
+			if i > 0 && strings.HasSuffix(parts[i-1], "Mo") && strings.HasPrefix(parts[i], "ay") {
+				parts[i-1] = parts[i-1] + "nd" + parts[i]
+				copy(parts[i:], parts[i+1:])
+				parts = parts[:len(parts)-1]
+			} else {
+				i++
+			}
+		}
 		a := make([]string, 0, 2*len(parts)-1)
 		for i, p := range parts {
 			if i > 0 {
---
+++ parse_test.go
@@ -0,0 +1,242 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package date
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAutoParse(t *testing.T) {
+	cases := []struct {
+		value string
+		year  int
+		month time.Month
+		day   int
+	}{
+		{"31/12/1969", 1969, time.December, 31},
+		{"1969/12/31", 1969, time.December, 31},
+		{"1969.12.31", 1969, time.December, 31},
+		{"1969-12-31", 1969, time.December, 31},
+		{"+1970-01-01", 1970, time.January, 1},
+		{"+01970-01-02", 1970, time.January, 2},
+		{"2000-02-28", 2000, time.February, 28},
+		{"+2000-02-29", 2000, time.February, 29},
+		{"+02000-03-01", 2000, time.March, 1},
+		{"+002004-02-28", 2004, time.February, 28},
+		{"2004-02-29", 2004, time.February, 29},
+		{"2004-03-01", 2004, time.March, 1},
+		{"0000-01-01", 0, time.January, 1},
+		{"+0001-02-03", 1, time.February, 3},
+		{"+00019-03-04", 19, time.March, 4},
+		{"0100-04-05", 100, time.April, 5},
+		{"2000-05-06", 2000, time.May, 6},
+		{"+5000000-08-09", 5000000, time.August, 9},
+		{"-0001-09-11", -1, time.September, 11},
+		{"-0019-10-12", -19, time.October, 12},
+		{"-00100-11-13", -100, time.November, 13},
+		{"-02000-12-14", -2000, time.December, 14},
+		{"-30000-02-15", -30000, time.February, 15},
+		{"-0400000-05-16", -400000, time.May, 16},
+		{"-5000000-09-17", -5000000, time.September, 17},
+		{"12340506", 1234, time.May, 6},
+		{"+12340506", 1234, time.May, 6},
+		{"-00191012", -19, time.October, 12},
+	}
+	for _, c := range cases {
+		d := MustAutoParse(c.value)
+		year, month, day := d.Date()
+		if year != c.year || month != c.month || day != c.day {
+			t.Errorf("ParseISO(%v) == %v, want (%v, %v, %v)", c.value, d, c.year, c.month, c.day)
+		}
+	}
+
+	badCases := []string{
+		"1234-05",
+		"1234-5-6",
+		"1234-05-6",
+		"1234-5-06",
+		"1234-0A-06",
+		"1234-05-0B",
+		"1234-05-06trailing",
+		"padding1234-05-06",
+		"1-02-03",
+		"10-11-12",
+		"100-02-03",
+		"+1-02-03",
+		"+10-11-12",
+		"+100-02-03",
+		"-123-05-06",
+	}
+	for _, c := range badCases {
+		d, err := AutoParse(c)
+		if err == nil {
+			t.Errorf("ParseISO(%v) == %v", c, d)
+		}
+	}
+}
+
+func TestParseISO(t *testing.T) {
+	cases := []struct {
+		value string
+		year  int
+		month time.Month
+		day   int
+	}{
+		{"1969-12-31", 1969, time.December, 31},
+		{"+1970-01-01", 1970, time.January, 1},
+		{"+01970-01-02", 1970, time.January, 2},
+		{"2000-02-28", 2000, time.February, 28},
+		{"+2000-02-29", 2000, time.February, 29},
+		{"+02000-03-01", 2000, time.March, 1},
+		{"+002004-02-28", 2004, time.February, 28},
+		{"2004-02-29", 2004, time.February, 29},
+		{"2004-03-01", 2004, time.March, 1},
+		{"0000-01-01", 0, time.January, 1},
+		{"+0001-02-03", 1, time.February, 3},
+		{"+00019-03-04", 19, time.March, 4},
+		{"0100-04-05", 100, time.April, 5},
+		{"2000-05-06", 2000, time.May, 6},
+		{"+30000-06-07", 30000, time.June, 7},
+		{"+400000-07-08", 400000, time.July, 8},
+		{"+5000000-08-09", 5000000, time.August, 9},
+		{"-0001-09-11", -1, time.September, 11},
+		{"-0019-10-12", -19, time.October, 12},
+		{"-00100-11-13", -100, time.November, 13},
+		{"-02000-12-14", -2000, time.December, 14},
+		{"-30000-02-15", -30000, time.February, 15},
+		{"-0400000-05-16", -400000, time.May, 16},
+		{"-5000000-09-17", -5000000, time.September, 17},
+		{"12340506", 1234, time.May, 6},
+		{"+12340506", 1234, time.May, 6},
+		{"-00191012", -19, time.October, 12},
+	}
+	for _, c := range cases {
+		d := MustParseISO(c.value)
+		year, month, day := d.Date()
+		if year != c.year || month != c.month || day != c.day {
+			t.Errorf("ParseISO(%v) == %v, want (%v, %v, %v)", c.value, d, c.year, c.month, c.day)
+		}
+	}
+
+	badCases := []string{
+		"1234-05",
+		"1234-5-6",
+		"1234-05-6",
+		"1234-5-06",
+		"1234/05/06",
+		"1234-0A-06",
+		"1234-05-0B",
+		"1234-05-06trailing",
+		"padding1234-05-06",
+		"1-02-03",
+		"10-11-12",
+		"100-02-03",
+		"+1-02-03",
+		"+10-11-12",
+		"+100-02-03",
+		"-123-05-06",
+	}
+	for _, c := range badCases {
+		d, err := ParseISO(c)
+		if err == nil {
+			t.Errorf("ParseISO(%v) == %v", c, d)
+		}
+	}
+}
+
+func BenchmarkParseISO(b *testing.B) {
+	cases := []struct {
+		layout string
+		value  string
+		year   int
+		month  time.Month
+		day    int
+	}{
+		{ISO8601, "1969-12-31", 1969, time.December, 31},
+		{ISO8601, "2000-02-28", 2000, time.February, 28},
+		{ISO8601, "2004-02-29", 2004, time.February, 29},
+		{ISO8601, "2004-03-01", 2004, time.March, 1},
+		{ISO8601, "0000-01-01", 0, time.January, 1},
+		{ISO8601, "0001-02-03", 1, time.February, 3},
+		{ISO8601, "0100-04-05", 100, time.April, 5},
+		{ISO8601, "2000-05-06", 2000, time.May, 6},
+	}
+	for n := 0; n < b.N; n++ {
+		c := cases[n%len(cases)]
+		_, err := ParseISO(c.value)
+		if err != nil {
+			b.Errorf("ParseISO(%v) == %v", c.value, err)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	// Test ability to parse a few common date formats
+	cases := []struct {
+		layout string
+		value  string
+		year   int
+		month  time.Month
+		day    int
+	}{
+		{ISO8601, "1969-12-31", 1969, time.December, 31},
+		{ISO8601B, "19700101", 1970, time.January, 1},
+		{RFC822, "29-Feb-00", 2000, time.February, 29},
+		{RFC822W, "Mon, 01-Mar-04", 2004, time.March, 1},
+		{RFC850, "Wednesday, 12-Aug-15", 2015, time.August, 12},
+		{RFC1123, "05 Dec 1928", 1928, time.December, 5},
+		{RFC1123W, "Mon, 05 Dec 1928", 1928, time.December, 5},
+		{RFC3339, "2345-06-07", 2345, time.June, 7},
+	}
+	for _, c := range cases {
+		d := MustParse(c.layout, c.value)
+		year, month, day := d.Date()
+		if year != c.year || month != c.month || day != c.day {
+			t.Errorf("Parse(%v) == %v, want (%v, %v, %v)", c.value, d, c.year, c.month, c.day)
+		}
+	}
+
+	// Test inability to parse ISO 8601 expanded year format
+	badCases := []string{
+		"+1234-05-06",
+		"+12345-06-07",
+		"-1234-05-06",
+		"-12345-06-07",
+	}
+	for _, c := range badCases {
+		d, err := Parse(ISO8601, c)
+		if err == nil {
+			t.Errorf("Parse(%v) == %v", c, d)
+		}
+	}
+}
+
+func BenchmarkParse(b *testing.B) {
+	// Test ability to parse a few common date formats
+	cases := []struct {
+		layout string
+		value  string
+		year   int
+		month  time.Month
+		day    int
+	}{
+		{ISO8601, "1969-12-31", 1969, time.December, 31},
+		{ISO8601, "2000-02-28", 2000, time.February, 28},
+		{ISO8601, "2004-02-29", 2004, time.February, 29},
+		{ISO8601, "2004-03-01", 2004, time.March, 1},
+		{ISO8601, "0000-01-01", 0, time.January, 1},
+		{ISO8601, "0001-02-03", 1, time.February, 3},
+		{ISO8601, "0100-04-05", 100, time.April, 5},
+		{ISO8601, "2000-05-06", 2000, time.May, 6},
+	}
+	for n := 0; n < b.N; n++ {
+		c := cases[n%len(cases)]
+		_, err := Parse(c.layout, c.value)
+		if err != nil {
+			b.Errorf("Parse(%v) == %v", c.value, err)
+		}
+	}
+}
