93168f6141f541552d32309f0f1ec5f2ac9c4097
Rick Beton <rick.beton@infosum.com>
2018-11-15 11:31:41 +0000

    more comments

--- gregorian/doc.go
+++ gregorian/doc.go
@@ -3,4 +3,14 @@
 // license that can be found in the LICENSE file.
 
 // Package gregorian provides utility functions for the Gregorian calendar calculations.
+// The Gregorian calendar was officially introduced in October 1582 so, strictly speaking,
+// it only applies after that date. Some countries did not switch to the Gregorian calendar
+// for many years after (such as Great Britain in 1782).
+//
+// Extending the Gregorian calendar backwards to dates preceding its official introduction
+// produces a proleptic calendar that should be used with some caution for historic dates
+// because it can lead to confusion.
+//
+// See https://en.wikipedia.org/wiki/Gregorian_calendar
+// https://en.wikipedia.org/wiki/Proleptic_Gregorian_calendar
 package gregorian
