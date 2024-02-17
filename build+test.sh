0872832ffefa29b1eb7a6f99b3e320b1e427ed46
Rick <rick.beton@gmail.com>
2024-02-17 18:51:49 +0000

    updated dependencies; simpler build script

--- build+test.sh
+++ build+test.sh
@@ -11,33 +11,22 @@ function v
   $@
 }
 
-if ! type -p goveralls; then
-  v go install github.com/mattn/goveralls
-fi
-
 if ! type -p shadow; then
   v go get     golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
   v go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
 fi
 
-if ! type -p goreturns; then
-  v go get     github.com/sqs/goreturns
-  v go install github.com/sqs/goreturns
-fi
-
 echo date...
 v go test -v -covermode=count -coverprofile=date.out .
 v go tool cover -func=date.out
-#[ -z "$COVERALLS_TOKEN" ] || goveralls -coverprofile=date.out -service=travis-ci -repotoken $COVERALLS_TOKEN
 
 for d in clock period timespan view; do
   echo $d...
   v go test -v -covermode=count -coverprofile=$d.out ./$d
   v go tool cover -func=$d.out
-  #[ -z "$COVERALLS_TOKEN" ] || goveralls -coverprofile=$d.out -service=travis-ci -repotoken $COVERALLS_TOKEN
 done
 
-v goreturns -l -w *.go */*.go
+v gofmt -l -w *.go */*.go
 
 v go vet ./...
 
--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/rickb777/date
 require (
 	github.com/onsi/gomega v1.27.10
 	github.com/rickb777/plural v1.4.1
-	golang.org/x/text v0.13.0
+	golang.org/x/text v0.14.0
 )
 
 require (
 	github.com/google/go-cmp v0.5.9 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/tools v0.18.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
